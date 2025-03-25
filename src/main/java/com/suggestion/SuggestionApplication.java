05a0dc9a362044314f7823335bcf9869540082be
Ahmad Safei Ridwan <ahmad.safei@autopedia.id>
2025-03-26 05:46:18 +0700

    init project

---
+++ src/main/java/com/suggestion/SuggestionApplication.java
@@ -0,0 +1,13 @@
+package com.suggestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuggestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuggestionApplication.class, args);
+	}
+
+}
